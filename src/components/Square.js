855fee73def7a57ae84ddcdd9a343a70ebbf5e58
TrungKien <nightstealth99@gmail.com>
2021-02-16 16:54:29 +0700

    finish board

---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './components.css'
+export default class Square extends React.Component{
+    render(){
+        return (
+        <button
+            className={"square " + this.props.shade}
+            onClick={this.props.onClick}
+        >
+        </button>
+        );  
+    }
+}
